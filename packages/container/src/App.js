36a72cd7477f22e16fe96b5c78af33fbb60dcf45
Rafail Zorzos <rafail.zorzos@gmail.com>
2023-03-17 15:46:00 +0200

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+	return (
+		<div>
+			<h1>Container!</h1>
+			<hr />
+			<div>
+				<MarketingApp />
+			</div>
+		</div>
+	);
+};
