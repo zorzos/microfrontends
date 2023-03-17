1878eb69598b994f2b5b179e431837d0b90c124b
Rafail Zorzos <rafail.zorzos@gmail.com>
2023-03-17 16:43:22 +0200

    Update MarketingApp.js

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect } from "react";
 import { mount } from "marketing/MarketingApp";
 
+console.log("RAFAIL ZORZOS");
+
 export default () => {
 	const ref = useRef(null);
 
