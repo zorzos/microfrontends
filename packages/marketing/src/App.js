ef93ff7b828ca2537bced28457c495073de49d60
Rafail Zorzos <rafail.zorzos@gmail.com>
2023-03-20 13:33:09 +0200

    Prod changes

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import {
+	StylesProvider,
+	createGenerateClassName,
+} from "@material-ui/core/styles";
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
+
+const generateClassName = createGenerateClassName({
+	productionPrefix: "ma",
+});
 
 export default () => {
-    return ( 
-        <div>
-            <StylesProvider>
-                <BrowserRouter>
-                    <Switch>
-                        <Route exact path="/pricing" component={Pricing} />
-                        <Route path="/" component={Landing} />
-                    </Switch>
-                </BrowserRouter>
-            </StylesProvider>
-        </div>
-    )
+	return (
+		<div>
+			<StylesProvider generateClassName={generateClassName}>
+				<BrowserRouter>
+					<Switch>
+						<Route
+							exact
+							path="/pricing"
+							component={Pricing}
+						/>
+						<Route
+							path="/"
+							component={Landing}
+						/>
+					</Switch>
+				</BrowserRouter>
+			</StylesProvider>
+		</div>
+	);
 };
