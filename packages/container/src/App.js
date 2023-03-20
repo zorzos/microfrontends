942217bcdf519b902f632433da07c82ecf72facf
Rafail Zorzos <rafail.zorzos@gmail.com>
2023-03-20 12:18:12 +0200

    More changes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
 	return (
-		<div>
-			<h1>Container!</h1>
-			<hr />
+		<BrowserRouter>
 			<div>
+				<Header />
 				<MarketingApp />
 			</div>
-		</div>
+		</BrowserRouter>
 	);
 };
